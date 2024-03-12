1aa9cd06cd2b44df37107f42690024bb2d0f92e2
cleverson <cleverson461@gmail.com>
2024-03-12 14:41:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const nome = 'Cleverson';
+let valor1 = 123;
+var verdadeiroOuFalso = true;
+const null1 = null;
+let undefined1;
+
+console.log(typeof nome) // String
+console.log(typeof valor1) // Number
+console.log(typeof verdadeiroOuFalso) // Boolean
+console.log(null1) // null
+console.log(typeof undefined1) // undefined
